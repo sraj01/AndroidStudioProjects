02f40c66f4d77d899e0aaa3b6a9cf9da583e96ce
Shivam Raj <dubey27072005@gmail.com>
2025-08-11 19:24:09 +0530

    Add AndroidDay11 Demo project

---
+++ Androiday11Demo/app/src/main/java/com/example/androiday11demo/SecondActivity.kt
@@ -0,0 +1,28 @@
+package com.example.androiday11demo
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_second)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.second_main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val username = intent.getStringExtra("username")
+        val textViewUser = findViewById<TextView>(R.id.textViewUser)
+        textViewUser.text = "Hello, $username!"
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container,MyFragment())
+            .commit()
+    }
+}
